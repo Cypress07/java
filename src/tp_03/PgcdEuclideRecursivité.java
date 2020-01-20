809403065d437f14863f85c7505e829ce9182011
MIKAEL OUZERA <m.ouzera@free.fr>
2020-01-20 14:34:19 +0100

    EXO PGCD RECURSIVITE

---
+++ src/tp_03/PgcdEuclideRecursivité.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author formation_gep
+ */
+public class PgcdEuclideRecursivité {
+
+     public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner reader = new Scanner(System.in);    
+   
+    System.out.print ("Ecrivez un entier:");
+    int a = reader.nextInt();    
+    
+    System.out.print ("Ecrivez un entier:");
+    int b = reader.nextInt();
+    
+    System.out.println("Le pgcd de "+a +" et "+b+" est : " + pgcd (a,b));
+  }
+    
+    public static int pgcd (int a, int b) {
+
+    int r;
+  
+    while (b > 0) {
+        r = a % b;
+        a = b;
+        b = r;
+  }
+        return a;
+
+}
+}
+       
