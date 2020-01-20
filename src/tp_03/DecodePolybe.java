310145db606aabade912c7ea714d93d5c503e7fa
mika <mika@192.168.1.11>
2020-01-20 19:10:18 +0100

    Polybe en COURS

--- src/tp_03/DecodePolybe.java
+++ src/tp_03/DecodePolybe.java
@@ -32,19 +32,19 @@ public class DecodePolybe {
         } ;
    //affichage de matrice
    
-    for (i=0; i<N ; i++) {
+    for (i=0; i < N ; i++) {
     System.out.println();
-    for (j=0; j<N ;j++){
+    for (j=0; j < N ;j++){
     System.out.print (tab [i][j] + " ");
         }
+    System.out.println(+Polybe (i,j));
     }
     }
 
     public static String Polybe (String mot) {
     
     int i, j, k=0,c;
-    char lettre;
-    string mot;     
+    char lettre;     
     final int N=5 ;
     char [][] tab= new char[][]{
         {'A','B','C','D','E'},
@@ -53,17 +53,38 @@ public class DecodePolybe {
         {'P','Q','R','S','T'},
         {'U','V','X','Y','Z'},
         } ;
-    mot [k]= lettre;
-    while (k < mot.length) { 
+    mot=mot.toLowerCase();
+            mot=mot.replace('w','v');
+            mot=mot.replace('ç','c');
+            mot=mot.replace('é','e');
+            mot=mot.replace('è','e');
+            mot=mot.replace('à','a');
+            mot=mot.replace('ù','u');
+            mot=mot.replaceAll("0", "zeros ");
+            mot=mot.replaceAll("1", "un ");
+            mot=mot.replaceAll("2", "deux ");
+            mot=mot.replaceAll("3", "trois ");
+            mot=mot.replaceAll("4", "quatre ");
+            mot=mot.replaceAll("5", "cinq ");
+            mot=mot.replaceAll("6", "six ");
+            mot=mot.replaceAll("7", "sept ");
+            mot=mot.replaceAll("8", "huit ");
+            mot=mot.replaceAll("9", "neuf ");
+            mot=mot.replaceAll("@", "at");
+            mot=mot.replaceAll("&", "et");
+            mot=mot.replaceAll("%", "pourcent ");
+    
+    lettre == mot [k] ;
+    while (k < mot.length()) { 
         for (i=0; i<N; i++){
         for (j=0; j<N; j++){
-            if lettre = (T[i][j]);
-            add i to c ;
-            add j t c ;
+            if (lettre == (tab[i][j]));
+            i += c ;
+            j += c ;
     } 
     }
-    k=k+1;
+    k=k+1;  
 }
-    return c;
+    return mot ;
     }
 }
