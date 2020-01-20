9c344b138058b67631e635abe34a3ff442f23f71
mika <mika@192.168.1.11>
2020-01-20 21:20:09 +0100

    ENCODE POLYBE v1.0

---
+++ src/tp_03/EncodePolybe.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_03;
+ import java.util.Scanner;
+/**
+ *
+ * @author mika
+ */
+public class EncodePolybe {
+    
+   public static void main(String[] args)
+    {
+        Scanner reader = new Scanner(System.in);    
+
+        String message = new String ();
+         
+        System.out.print("Entrez votre message : \n\t");
+        message = reader.nextLine();
+         
+        //test de la fonction :
+        System.out.print(" "+polybe(message));
+    }
+   public static String polybe(String message) {
+          
+        String messageCrypte = "";
+        final int N=5;
+        char [][] tab;
+        tab = new char[][] {
+         
+            {'a','b','c','d','e'},
+            {'f','g','h','i','j'},
+            {'k','l','m','n','o'},
+            {'p','q','r','s','t'},
+            {'u','v','x','y','z'},
+            
+        };
+ 
+            message=message.toLowerCase();
+            message=message.replace('w','v');
+            message=message.replace('ç','c');
+            message=message.replace('é','e');
+            message=message.replace('è','e');
+            message=message.replace('à','a');
+            message=message.replace('ù','u');
+            message=message.replaceAll("0", "zeros ");
+            message=message.replaceAll("1", "un ");
+            message=message.replaceAll("2", "deux ");
+            message=message.replaceAll("3", "trois ");
+            message=message.replaceAll("4", "quatre ");
+            message=message.replaceAll("5", "cinq ");
+            message=message.replaceAll("6", "six ");
+            message=message.replaceAll("7", "sept ");
+            message=message.replaceAll("8", "huit ");
+            message=message.replaceAll("9", "neuf ");
+            message=message.replaceAll("@", "at");
+            message=message.replaceAll("&", "et");
+            message=message.replaceAll("%", "pourcent ");
+             
+            //cryptage du message :
+            for (int i = 0; i<message.length(); i++)
+                for(int j = 0; j<N; j++)
+                    for(int k = 0; k<N; k++)
+                        if((message.charAt(i))==(tab[j][k]))
+                        {
+                            messageCrypte+=(j);
+                            messageCrypte+=(k);
+                            messageCrypte+=' ';
+                        }
+ 
+            return messageCrypte;
+        }
+ 
+    }
